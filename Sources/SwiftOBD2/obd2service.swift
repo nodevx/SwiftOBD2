cd1e87dda0b5d3d0d89f1040e4a3c85607c46e76
Mikhail Nikitsin <developer.nikitsin@gmail.com>
2025-10-22 17:45:42 +0200

    Add temporary fix for connectionType change (BLE)

--- Sources/SwiftOBD2/obd2service.swift
+++ Sources/SwiftOBD2/obd2service.swift
@@ -52,6 +52,8 @@ public class OBDService: ObservableObject, OBDServiceDelegate {
             ConfigurationService.shared.connectionType = connectionType
         }
     }
+    
+    private var bleManager: BLEManager?
 
     /// The internal ELM327 object responsible for direct adapter interaction.
     private var elm327: ELM327
@@ -70,8 +72,9 @@ public class OBDService: ObservableObject, OBDServiceDelegate {
 #else
         switch connectionType {
         case .bluetooth:
-            let bleManager = BLEManager()
-            elm327 = ELM327(comm: bleManager)
+            let manager = bleManager ?? BLEManager()
+            bleManager = manager
+            elm327 = ELM327(comm: manager)
         case .wifi:
             elm327 = ELM327(comm: WifiManager())
         case .demo:
